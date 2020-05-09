e19caec73118575518ea9284eeb0163fac547ad3
jotarun <jotarun@gmail.com>
2020-05-08 22:36:51 -0700

    add card search 1.0

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const parser = require("discord-command-parser");
-const DeckDecoder = require('./deck.js')
+const DeckUtil = require('./deck.js')
 const prefix = "!";
 
 const client = new Discord.Client();
@@ -15,17 +15,49 @@ client.on('ready', () => {
     regionEmoji[4] = client.emojis.cache.find(emoji => emoji.name === 'piltoverzaun');
     regionEmoji[5] = client.emojis.cache.find(emoji => emoji.name === 'shadowisles');
     regionEmoji[6] = client.emojis.cache.find(emoji => emoji.name === 'bilgewater');
+  
 });
 
 
 client.on('message', message => {
+    const deckUtil = new DeckUtil();
     const parsed = parser.parse(message, prefix);
     if (!parsed.success) return;
 
+    if (parsed.command === "查詢") {
+        cardname = parsed.arguments[0];
+        card = deckUtil.search(cardname);
+        if (Object.keys(card).length === 0)
+        {
+            return message.reply("找不到這張卡喔");
+        }
+        else
+        {
+            console.log(card.cardCode);
+
+        const cardEmbed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle(card.name)
+        .setDescription(card.flavorText)
+        .setImage(card.assets[0].gameAbsolutePath);
+        message.channel.send(cardEmbed);
+
+        if (card.supertype=="英雄")
+            {
+                let lvcard = deckUtil.getlevelupcard(card);
+                const cardEmbed = new Discord.MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle(lvcard.name)
+                .setDescription(lvcard.flavorText)
+                .setImage(lvcard.assets[0].gameAbsolutePath);
+                message.channel.send(cardEmbed);
+            }
+        }
+
+    }
     if (parsed.command === "牌組") {
-        const deckDecoder = new DeckDecoder();
         code = parsed.arguments[0]
-        let result = deckDecoder.decode(code);
+        let result = deckUtil.decode(code);
         if (Object.keys(result).length === 0) {
             return message.reply("代碼有錯喔");
 
--- deck.js
+++ deck.js
@@ -2,31 +2,63 @@ const { DeckEncoder } = require('runeterra');
 const set1 = require('./cards/set1/zh_tw/data/set1-zh_tw.json');
 const set2 = require('./cards/set2/zh_tw/data/set2-zh_tw.json');
 
-class DeckDecoder {
+class DeckUtil {
+
+  constructor() {
+    this.cardSets = {};
+    this.cardSets[1] = set1;
+    this.cardSets[2] = set2;
+  }
+
+  getlevelupcard(card) {
+    let set = parseInt(card.cardCode.substring(0, 2));
+    let name = card.name;
+    let lvlupcard = {};
+    let index = 0;
+    card.associatedCardRefs.forEach(cardCode => {
+      index = this.cardSets[set].findIndex(obj =>
+        obj.cardCode == cardCode);
+      if (this.cardSets[set][index].name == name) {
+        console.log(name);
+        lvlupcard = this.cardSets[set][index];
+      }
+    });
+    return lvlupcard;
+  }
+
+  search(cardname) {
+    let card = {};
+    for (let set = 1; set < 2; set++) {
+      let index = this.cardSets[set].findIndex(obj => obj.name == cardname);
+      if (index >= 0) {
+        card = this.cardSets[set][index];
+        break;
+      }
+    }
+    return card;
+  }
 
   decode(deckcode) {
-    let cardSets = {};
-    cardSets[1] = set1;
-    cardSets[2] = set2;
+
     let deck = {};
     let heroes = {};
     let minions = {};
     let spells = {};
     try {
 
-    deck = DeckEncoder.decode(deckcode);
-  }
-  catch(err) {
-    return{};
-  }
+      deck = DeckEncoder.decode(deckcode);
+    }
+    catch (err) {
+      return {};
+    }
     deck.forEach((o, i, a) => {
       let set = a[i].set;
-      let index = cardSets[set].findIndex(obj => obj.cardCode == a[i].code);
-      a[i].supertype = cardSets[set][index].supertype;
-      a[i].type = cardSets[set][index].type;
-      a[i].cost = cardSets[set][index].cost;
-      a[i].name = cardSets[set][index].name;
-      a[i].assets = cardSets[set][index].assets;
+      let index = this.cardSets[set].findIndex(obj => obj.cardCode == a[i].code);
+      a[i].supertype = this.cardSets[set][index].supertype;
+      a[i].type = this.cardSets[set][index].type;
+      a[i].cost = this.cardSets[set][index].cost;
+      a[i].name = this.cardSets[set][index].name;
+      // a[i].assets = this.cardSets[set][index].assets;
 
     });
     heroes = deck.filter(card => card.supertype == "英雄");
@@ -46,4 +78,4 @@ class DeckDecoder {
     });
   }
 };
-module.exports = DeckDecoder;
+module.exports = DeckUtil;
